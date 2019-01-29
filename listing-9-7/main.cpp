1d491e80d6478850b947f3bbd88751d6bc02d9ed
splitfire <guangjian.yang@outlook.com>
2019-01-29 15:45:14 +0800

    update(listing-9-7):move ctor and move assignment op.

--- listing-9-7/main.cpp
+++ listing-9-7/main.cpp
@@ -91,7 +91,7 @@ private:
 	char* buffer;
 
 public:
-	MyString() {}
+	MyString() : buffer(NULL) {}
 	MyString(const char* initString) // constructor
 	{
 		buffer = NULL;
@@ -127,11 +127,15 @@ public:
 	~MyString()
 	{
 		cout << "Invoking destructor, clearing up" << endl;
-		delete[] buffer;
+		if (buffer)
+		{
+			delete[] buffer;
+		}
 	}
 
 	int GetLength()
 	{
+		//默认构造函数会跳过buffer的赋值,得到NULL,而strlen(buffer)是非法的
 		return strlen(buffer);
 	}
 
@@ -139,6 +143,108 @@ public:
 	{
 		return buffer;
 	}
+
+#if 0
+	MyString(MyString&& moveSrc) // move constructor
+	{
+		cout << "Move constructor moves: " << moveSrc.buffer << endl;
+		if (moveSrc.buffer != NULL)
+		{
+			buffer = moveSrc.buffer; // take ownership i.e.  'move'
+			moveSrc.buffer = NULL;   // free move source
+		}
+	}
+
+	MyString& operator= (MyString&& moveSrc) // move assignment op.
+	{
+		cout << "Move assignment op. moves: " << moveSrc.buffer << endl;
+		if ((moveSrc.buffer != NULL) && (this != &moveSrc))
+		{
+			delete[] buffer; // release own buffer
+
+			buffer = moveSrc.buffer; // take ownership i.e.  'move'
+			moveSrc.buffer = NULL;   // free move source
+		}
+
+		return *this;
+	}
+
+	MyString operator+ (const MyString& addThis)
+	{
+		cout << "operator+ called: " << endl;
+		MyString newStr;
+
+		if (addThis.buffer != NULL)
+		{
+			newStr.buffer = new char[GetLength() + strlen(addThis.buffer) + 1];
+			strcpy(newStr.buffer, buffer);
+			strcat(newStr.buffer, addThis.buffer);
+		}
+
+		return newStr;
+	}
+
+	MyString& operator= (const MyString& copySrc) // Copy assignment op.
+	{
+		cout << "Copy assignment op. copies: " << copySrc.buffer << endl;
+		if ((this != &copySrc) && (copySrc.buffer != NULL))
+		{
+			if (buffer != NULL)
+				delete[] buffer;
+
+			buffer = new char[strlen(copySrc.buffer) + 1];
+			strcpy(buffer, copySrc.buffer);
+		}
+
+		return *this;
+	}
+#else
+	// 以上为书中源码,以下为自己写的
+	MyString(MyString&& moveSrc)
+	{
+		if (moveSrc.buffer != NULL)
+		{
+			//delete buffer; //移动构造中不能delete
+			buffer = moveSrc.buffer;
+			moveSrc.buffer = NULL;
+		}
+	}
+
+	//如果等号是右侧
+	MyString& operator=(MyString&& moveSrc) //数学语义:连等,所以返回引用
+	{
+		cout << "move assignment op. called" << endl;
+
+		if (this != &moveSrc &&
+			moveSrc.buffer != NULL)
+		{
+			if (buffer)
+			{
+				delete buffer;
+			}
+
+			buffer = moveSrc.buffer;
+			moveSrc.buffer = NULL;
+		}
+		//=右侧为临时变量
+		return *this;
+	}
+
+	MyString operator+(const MyString& addThis) //数学语义:不保存到第一个加数,所以按指返回
+	{
+		MyString tmp;
+
+		if (addThis.buffer != NULL)
+		{
+			tmp.buffer = new char[GetLength() +  strlen(addThis.buffer) + 1]; //this buffer may null
+			strcpy(tmp.buffer, buffer);
+			strcat(tmp.buffer, addThis.buffer);
+		}
+
+		//局部变量构造临时变量返回，编译器知道局部变量即将析构，选择移动构造
+		return tmp;
+	}
+#endif
 };
 
 void UseMyString(MyString& str)
@@ -158,8 +264,8 @@ MyString CopySring(MyString& src)
 
 int main()
 {
-	MyString sayHello("Hello from String Class");
-	UseMyString(sayHello);
+	//MyString sayHello("Hello from String Class");
+	//UseMyString(sayHello);
 
 	{
 		/*
@@ -167,11 +273,21 @@ int main()
 		 * 答： 这是编译器对复制构造函数的要求。其原因是，如果按值接受源对象，复制构造函数将调用
 		 * 自己，导致没完没了的复制循环。
 		 */
-		MyString str1;
-		CopySring(str1);
+		//MyString str1("Hello World");
+		//CopySring(str1);
+		//MyString str2(CopySring(str1));
 
-		 //移动构造函数
+		 // 移动构造函数
+		 // 我们发现移动构造触发时机:局部变量,临时变量
+		MyString Hello("Hello ");
+		MyString World("World");
+		MyString CPP(" of C++");
 
+		MyString sayHelloAgain("overwrite this");
+		sayHelloAgain = Hello + World + CPP;
+
+//		MyString emptyStr;
+//		MyString anothorPlus = emptyStr + Hello;
 	}
 
 	return 0;
